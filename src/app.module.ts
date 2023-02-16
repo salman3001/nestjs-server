e63cb7ca314b0d25b03ad156d693effbe72d13bc
Salman <therodfighter@gmail.com>
2023-02-16 01:26:57 -0800

    added connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.BUYZONE_DB_URI, {
+      dbName: 'buyzone',
+      connectionName: 'buyzone',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
