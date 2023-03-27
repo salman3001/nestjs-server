2587fbc8c6fcd874be570090df6fd166b5bbc5e9
salman <therodfighter@gmail.com>
2023-03-27 23:52:41 +0400

    added github pages to corse

--- apps/buyzone/src/main.ts
+++ apps/buyzone/src/main.ts
@@ -23,6 +23,7 @@ async function bootstrap() {
       'http://localhost:3000',
       'http://localhost:4000',
       'http://localhost:5173',
+      'https://salman3001.github.io',
     ],
     credentials: true,
   });
